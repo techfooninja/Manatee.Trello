c0238114ee7f321d22a41dd26c730afd4cc90241
Josh Davis <josh@davis.fm>
2021-02-18 13:57:31 -0600

    Add logic to handle unknown notification types without throwing an exception

--- Manatee.Trello/Json/Entities/ManateeNotification.cs
+++ Manatee.Trello/Json/Entities/ManateeNotification.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using Manatee.Json;
 using Manatee.Json.Serialization;
 
@@ -21,7 +22,31 @@ namespace Manatee.Trello.Json.Entities
 			MemberCreator = obj.Deserialize<IJsonMember>(serializer, "idMemberCreator");
 			Data = obj.Deserialize<IJsonNotificationData>(serializer, "data");
 			Unread = obj.TryGetBoolean("unread");
-			Type = obj.Deserialize<NotificationType?>(serializer, "type");
+
+			try
+			{
+				Type = obj.Deserialize<NotificationType?>(serializer, "type");
+			}
+			catch (TargetInvocationException tie)
+			{
+				bool shouldThrow = true;
+
+				if (tie.InnerException != null && tie.InnerException is ArgumentException ae)
+				{
+					if (!string.IsNullOrWhiteSpace(ae.Message) && ae.Message.StartsWith("Requested value '") && ae.Message.EndsWith("' was not found."))
+					{
+						// Error parsing notification type - default to Unknown
+						shouldThrow = false;
+						Type = NotificationType.Unknown;
+					}
+				}
+
+				if (shouldThrow)
+				{
+					throw;
+				}
+			}
+
 			Date = obj.Deserialize<DateTime?>(serializer, "date");
 		}
 		public JsonValue ToJson(JsonSerializer serializer)
